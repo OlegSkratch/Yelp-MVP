ea039281603aa6fcff41b1049dd5d07ee721de7b
oleg deru <olegderu98@gmail.com>
2022-02-16 15:01:53 +0100

    ok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :category, presence: true, acceptance: {accept %w[chinese italian japanes french belgian]}
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, numericality: {integer}
+  validates :rating, format: { with :/\A[0-5]/ }
 end
